544728d5ac939411ff908a2d314999f4a07fcd8c
teodorabombassaro-lgtm <teodora_bombassaro@estudante.sesisenai.org.br>
2026-03-25 14:18:36 +0000

    update

--- Lista 1 python/atividade23.py
+++ Lista 1 python/atividade23.py
@@ -1,4 +1,8 @@
 #Num dia de sol, você deseja medir a altura de um prédio, porém, a trena não é suficientemente longa.
 # Assumindo que seja possível medir sua sombra e a do prédio no chão, e que você lembre da sua altura, 
 #faça um algoritmo para ler os dados necessários e calcular a altura do prédio.
-altura = float (input("Qual a sua altura?"))
+altura_pessoa = float (input("Digite a sua altura"))
+sombra_pessoa = float (input("Digite o comprimento da sua sombra:"))
+sombra_predio = float (input("Dgite a sombra do predio:"))
+altura_predio = (altura_pessoa / sombra_pessoa) * sombra_predio
+print ("A altura do predio é", altura_predio)
