b617a2b2648defa1e70ac07f020e57b70ec87bc2
teodorabombassaro-lgtm <teodora_bombassaro@estudante.sesisenai.org.br>
2026-04-22 14:18:50 +0000

    update

--- Lista 1 python/atividade24.py
+++ Lista 1 python/atividade24.py
@@ -2,8 +2,9 @@
 #2 partes de suco de maracujá. 
 #Faça um algoritmo para calcular quantos litros de água e de suco são necessários para se fazer X litros de refresco 
 #(informados pelo usuário).
+total_partes = 10
 litros_refresco = float (input("Quantos litros de refresco você deseja fazer?"))
-litros_agua = litros_refresco * 0.8
-litros_suco = litros_refresco * 0.2
+litros_agua = (litros_refresco * 8) / total_partes
+litros_suco = (litros_refresco * 2) / total_partes
 total_litros = litros_agua + litros_suco
 print(f"Para fazer {litros_refresco} litros de refresco, precisamos de {litros_agua} litros de água e {litros_suco} litros de suco")
--- Lista 1 python/atividade25.py
+++ Lista 1 python/atividade25.py
@@ -1,6 +1,5 @@
 #Calcule o volume de uma caixa d'água cilíndrica.
 altura = float (input("Qual a medida em metros altura da caixa d'água?"))
 raio = float (input("Qual a medida em metros do raio da caixa d'água?"))
-conta = (3.14 * raio ** 2) * altura
-volume = conta * 1000
-print (f"O volume de uma caixa d'água cilíndrica é de: {volume} litros")
+volume = 3.14159 * (raio * 2) * altura
+print (f"O volume de uma caixa d'água cilíndrica é de: {volume:.2f} M³")
